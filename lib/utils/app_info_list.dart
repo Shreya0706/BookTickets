08b8a6111ae2e692850ff47cc1043e0cf4a2b932
shreya0706 <shreya.gupta2020@vitbhopal.ac.in>
2022-11-11 12:35:52 +0530

    Initial commit

---
+++ lib/utils/app_info_list.dart
@@ -0,0 +1,79 @@
+List<Map<String, dynamic>> hotelList =[
+  {
+    'image': 'one.jpg',
+    'place': 'Open Space',
+    'destination': 'London',
+    'price': '25',
+  },
+  {
+    'image': 'two.jpg',
+    'place': 'Global Will',
+    'destination': 'London',
+    'price': '40',
+  },
+  {
+    'image': 'three.jpg',
+    'place': 'Tallest Building',
+    'destination': 'Dubai',
+    'price': '68',
+  },
+];
+
+List<Map<String, dynamic>> ticketList =[
+  {
+    'from': {
+      'code': "NYC",
+      'name': "New-York"
+    },
+    'to': {
+      'code': "LDN",
+      'name': "London"
+    },
+    'flying_time': '8H 30M',
+    'date': '1 MAY',
+    'departure_time': "08:00 AM",
+    'number': 23
+  },
+  {
+    'from': {
+      'code': "DK",
+      'name': "Dhaka"
+    },
+    'to': {
+      'code': "SH",
+      'name': "Shanghai"
+    },
+    'flying_time': '4H 20M',
+    'date': '10 MAY',
+    'departure_time': "09:00 AM",
+    'number': 45
+  },
+  {
+    'from': {
+      'code': "DLH",
+      'name': "Delhi"
+    },
+    'to': {
+      'code': "BNG",
+      'name': "Banglore"
+    },
+    'flying_time': '13H 30M',
+    'date': '11 SEPT',
+    'departure_time': "01:20 PM",
+    'number': 42
+  },
+  {
+    'from': {
+      'code': "CHN",
+      'name': "Chennai"
+    },
+    'to': {
+      'code': "PNQ",
+      'name': "Pune"
+    },
+    'flying_time': '18H 40M',
+    'date': '1 MAY',
+    'departure_time': "06:29 PM",
+    'number': 39
+  },
+];
